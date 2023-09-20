c78260d27a2c6e4461b3ad82e9962e91a2bc1417
elizabeth2026 <elizabethl2026@student.cis.edu.hk>
2023-09-20 11:50:04 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,44 @@ public class Main {
 
     }
 
+
     // 1. add
-    
+    public static int add(int a, int b){
+        return a + b;
+    }
     // 2. add
+    public static int add(int a, int b, int c, int d){
+        return a+b+c+d;
+    }
 
     // 3. morningGreeting
+    public static String morningGreeting(String name) {
+        return ("早上好, " + name+"!");
+    }
 
     // 4. afternoonGreeting
+    public static String afternoonGreeting(String name) {
+    return ("下午好, "+name+"!");
+    }
 
     // 5. triple
+    public static String triple(String name) {
+    return (name+name+name);
+    }
 
     // 6. half
+    public static double half(double integer) {
+    return (integer/2);
+    }
 
     // 7. roundPositiveValueToNearestInteger
+    public static int roundPositiveValueToNearestInteger(double a){
+    return (int) (a+0.5);
+
+    }
 
     // 8. roundNegativeValueToNearestInteger
-
+    public static int roundNegativeValueToNearestInteger(double b) {
+    return (int) (b-0.5);
+    }
 }
